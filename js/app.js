b3fdbe748c3e9debbe92b8525169acd257600c46
RileyMoore01 <xrileyxmoorex@outlook.com>
2024-02-01 20:17:27 -0600

    cleaning up css

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
-document.getElementById("contact-form").addEventListener("submit", function(event){
-    event.preventDefault();
-    var formData = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
-    };
-    // Send this data to your server using fetch or XMLHttpRequest
-    fetch('/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-    })
-    .then(response => response.json())
-    .then(data => alert("Email sent successfully!"))
-    .catch((error) => {
-        console.error('Error:', error);
-        alert("Error sending email.");
-    });
-});
+// document.getElementById("contact-form").addEventListener("submit", function(event){
+//     event.preventDefault();
+//     var formData = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         subject: document.getElementById("subject").value,
+//         message: document.getElementById("message").value
+//     };
+//     // Send this data to your server using fetch or XMLHttpRequest
+//     fetch('/send-email', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//     })
+//     .then(response => response.json())
+//     .then(data => alert("Email sent successfully!"))
+//     .catch((error) => {
+//         console.error('Error:', error);
+//         alert("Error sending email.");
+//     });
+// });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlide");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
